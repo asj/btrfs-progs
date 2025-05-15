63bffb84275196aea5c113fcc259e730f1dc9741
Anand Jain <anand.jain@oracle.com>
2025-05-15 17:05:35 +0800

    btrfs-progs: fix inconsistency in device path handling
    There is an inconsistency in how device paths are handled within
    btrfs-progs. Specifically, the btrfs device command resolves and
    canonicalizes input device paths, while mkfs.btrfs does not.
    
    For example, running mkfs.btrfs /root/junk registers /root/junk in the
    kernel. This path remains even after mounting because kernel commit
    2e8b6bc0ab41 ("btrfs: avoid unnecessary device path update for the same
    device"), which was intended to prevent device path changes after mount,
    also unintentionally blocks valid device path updates during the mount
    process.
    
    As a result, show_devname() in btrfs returns a device path that doesn't
    match what libblkid or the mount command used. This mismatch leads to
    failures when mounting via UUIDs using mount -a, especially if /etc/fstab
    refers to UUIDs.
    
    This issue was originally reported on a production system running the UEK7
    kernel based on 5.15, where the commit 2e8b6bc0ab41 had been backported
    from the upstream stable-5.15.y branch. It has since been reproduced as
    fstests test case.
    
      $ ./check generic/768
      FSTYP         -- btrfs
      PLATFORM      -- Linux/x86_64 anajain-devoci 5.15.0-305.176.4.el9uek.x86_64
      MKFS_OPTIONS  -- /dev/sdc2
      MOUNT_OPTIONS -- /dev/sdc2 /mnt/scratch
    
      generic/768 5s ... - output mismatch
        --- tests/generic/768.out
        +++ generic/768.out.bad
        @@ -1,6 +1,6 @@
         QA output created by 768
         mount: MAPPER_PATH mounted on SCRATCH_MNT.
         MAPPER_PATH
        -MAPPER_PATH
        -MAPPER_PATH SCRATCH_MNT
        -SCRATCH_MNT             : already mounted
    
    The incorrect device path originates from `mkfs.btrfs`, which doesn’t
    canonicalize paths before passing them to the kernel. In contrast,
    `btrfs device` does. To avoid uncanonical paths like `/root/junk`
    persisting, make path handling consistent, `mkfs.btrfs` should
    canonicalize paths like `btrfs device`.
    
    Some tools may pass non-canonicalized paths via BTRFS_IOC_SCAN_DEV
    before mount, so the issue is not just limited to mkfs.btrfs.
    In such cases, show_devname() should reflect the mount-time path,
    as it does in the output of /proc/self/mounts for XFS and Ext4.
    Therefore, the kernel must be fixed as well.
    
    Also, libblkid can't detect mounts from symlinked paths it could have been
    smarter. It should resolve them and use major:minor to check mount status
    accurately.
    
    PS: Kernel commit `d565fffa6856` ("btrfs: do not skip re-registration
    for the mounted device") avoids the issue on single-device filesystems.
    However, the problem persists with multi-device filesystems. That commit
    was developed for the temp-fsid feature and does not fix this issue.
    
    Orabug: 37868203
    
    Link: https://lore.kernel.org/linux-btrfs/5f401c48-29f5-403e-8c39-50188028ad00@oracle.com/
    Link: https://lore.kernel.org/linux-btrfs/cover.1747729098.git.anand.jain@oracle.com/
    Reported-by: Ankur Lalwani <ankur.lalwani@oracle.com>
    Signed-off-by: Anand Jain <anand.jain@oracle.com>
    Tested-by: Ankur Lalwani <ankur.lalwani@oracle.com>

--- mkfs/main.c
+++ mkfs/main.c
@@ -1751,7 +1751,7 @@ int BOX_MAIN(mkfs)(int argc, char **argv)
 	}
 
 	for (i = 0; i < device_count; i++) {
-		file = argv[optind++];
+		file = path_canonicalize(argv[optind++]);
 
 		if (source_dir && path_exists(file) == 0)
 			ret = 0;
@@ -1767,7 +1767,7 @@ int BOX_MAIN(mkfs)(int argc, char **argv)
 	optind = saved_optind;
 	device_count = argc - optind;
 
-	file = argv[optind++];
+	file = path_canonicalize(argv[optind++]);
 	ssd = device_get_rotational(file);
 	if (opt_zoned) {
 		if (!zone_size(file)) {
@@ -1972,7 +1972,7 @@ int BOX_MAIN(mkfs)(int argc, char **argv)
 	for (i = saved_optind; i < saved_optind + device_count; i++) {
 		char *path;
 
-		path = argv[i];
+		path = path_canonicalize(argv[i]);
 		ret = test_minimum_size(path, min_dev_size);
 		if (ret < 0) {
 			error("failed to check size for %s: %m", path);
@@ -2036,7 +2036,8 @@ int BOX_MAIN(mkfs)(int argc, char **argv)
 	opt_oflags = O_RDWR;
 	for (i = 0; i < device_count; i++) {
 		if (opt_zoned &&
-		    zoned_model(argv[optind + i - 1]) == ZONED_HOST_MANAGED) {
+		    zoned_model(path_canonicalize(argv[optind + i - 1])) ==
+							ZONED_HOST_MANAGED) {
 			opt_oflags |= O_DIRECT;
 			break;
 		}
@@ -2044,7 +2045,7 @@ int BOX_MAIN(mkfs)(int argc, char **argv)
 
 	/* Start threads */
 	for (i = 0; i < device_count; i++) {
-		prepare_ctx[i].file = argv[optind + i - 1];
+		prepare_ctx[i].file = path_canonicalize(argv[optind + i - 1]);
 		prepare_ctx[i].byte_count = byte_count;
 		prepare_ctx[i].dev_byte_count = byte_count;
 		ret = pthread_create(&t_prepare[i], NULL, prepare_one_device,
@@ -2438,7 +2439,7 @@ out:
 		optind = saved_optind;
 		device_count = argc - optind;
 		while (device_count-- > 0) {
-			file = argv[optind++];
+			file = path_canonicalize(argv[optind++]);
 			if (path_is_block_device(file) == 1)
 				btrfs_register_one_device(file);
 		}
